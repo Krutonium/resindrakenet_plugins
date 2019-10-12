59309f4b72eb966a9ceb2722dc4871e2226fa436
Krutonium <PFCKrutonium@gmail.com>
2019-10-12 02:10:03 -0400

    Added Beginnings of a command.

--- HardcoreJobs/src/ga/krutonium/hardcorejobs/Main.java
+++ HardcoreJobs/src/ga/krutonium/hardcorejobs/Main.java
@@ -3,6 +3,10 @@ package ga.krutonium.hardcorejobs;
 import com.pablo67340.SQLiteLib.Main.SQLiteLib;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -12,7 +16,7 @@ import ga.krutonium.hardcorejobs.ValidJobs;
 import java.util.List;
 import java.util.logging.Logger;
 
-public class Main extends JavaPlugin implements Listener
+public class Main extends JavaPlugin implements Listener, CommandExecutor
 {
     private Logger log;
     @Override
@@ -25,6 +29,7 @@ public class Main extends JavaPlugin implements Listener
             log.info(s);
         }
         Bukkit.getServer().getPluginManager().registerEvents(this, this);
+        this.getCommand("job").setExecutor(this::onCommand);
     }
     @Override
     public void onDisable(){}
@@ -36,5 +41,14 @@ public class Main extends JavaPlugin implements Listener
             event.getPlayer().sendMessage("You're not a Miner");
             event.setCancelled(true);
         }
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        sender.sendMessage("You did it!");
+        return true;
+
+
+        //return super.onCommand(sender, command, label, args);
     }
 }
